1f9cf0e62ae270a655b1c6a1a45ea543e7c03ba3
Daniel <danielvargasvila09@hotmail.com>
2025-02-07 14:15:49 +0100

    maj

---
+++ README.md
@@ -0,0 +1,1 @@
+## Application Online Food Store
---
+++ frontend/src/app/submit/submit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit',
+  imports: [],
+  templateUrl: './submit.component.html',
+  styleUrl: './submit.component.css'
+})
+export class SubmitComponent {
+
+}
